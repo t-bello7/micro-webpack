56ee9c0f8156d4701c07cd04e027e7383c2227ad
t-bello7 <bello4aus@gmail.com>
2022-06-07 09:52:03 +0100

    add webpack-dev-server and webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 module.exports = {
     entry: './src/main.js',
-    plugins : [
+    plugins : [   
         new HtmlWebpackPlugin ({
             template: './src/index.html',
-        })
+        }), 
+        new BundleAnalyzerPlugin()
     ],
     output: {
         filename: 'output.bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    devServer : {
+        static: './dist',
+        port: 9000
+    },
+    mode: 'development'
 }
